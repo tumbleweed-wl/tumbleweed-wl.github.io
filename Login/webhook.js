f7b67c56fd83cc0544d1f0e81e2d181cecec78aa
tumbleweed-wl <554058483@qq.com>
2021-02-06 17:55:19 +0800

    CSS背景图随浏览器页面大小填充

--- Login/webhook.js
+++ Login/webhook.js
@@ -1,18 +1,14 @@
 const axios = require('axios');
 axios.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8c99cf61-bece-47d7-b326-18c6d4efdbbd', {
-    
-        "msgtype": "markdown",
-        "markdown": {
-          "content": " 2021/02/05/截止至22：00单据统计：\n>王磊：总单量：`31`单；\n>余小燕：总单量：`27` 单\n>周明利：总单量：`50` 单；\n>袁刚刚：总单量：`2`单；\n>李雄：总单量：`35`单，其中包含事件单据`16`单，项目单据`19`单；"
-          /*  /n 代表换行 不能直接换行 会报错
-               > 表示引用文字
-          */
-        }
-      
+
+  "msgtype": "markdown",
+  "markdown": {
+    "content": "### IT服务中心截止至22:00单据统计:\n>今日总单量：<font color=\"warning\">X</font>单；\n >跨部门：<font color=\"comment\">X</font>单；\n >下派区域：<font color=\"comment\">X</font>单；\n >同事1：经手单据:<font color=\"info\">X</font>单，\n >已完成：<font color=\"info\">X</font>单；\n >同事2：经手单据:<font color=\"info\">X</font>单，\n >已完成：<font color=\"info\">X</font>单；"
+  }
 })
-.then(function (response) {
+  .then(function (response) {
     console.log(response);
-})
-.catch(function (error) {
+  })
+  .catch(function (error) {
     console.log(error);
-});
+  });
