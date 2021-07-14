f2ce4bcd81baa1d63afcde862f869e20dbfb4e3a
tumbleweed-wl <554058483@qq.com>
2021-07-14 16:17:25 +0800

    0714

--- Login/webhook.js
+++ Login/webhook.js
@@ -3,7 +3,7 @@ axios.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8c99cf61-bece-4
 
   "msgtype": "markdown",
   "markdown": {
-    "content": "2020/03/05 IT服务中心截止至22:00单据统计:\n >周明利：已完成：<font color=\"info\">8</font>单；\n >王磊：已完成：<font color=\"info\">99</font>单；\n >史顺康：已完成：<font color=\"info\">47</font>单；"
+    "content": "IT服务中心截止至22:00单据统计:\n >史顺康：已完成：<font color=\"info\">123</font>单；\n >王磊：已完成：<font color=\"info\">112</font>单；\n >肖周荣：已完成：<font color=\"info\">34</font>单；\n >万文韬：已完成：<font color=\"info\">6</font>单；"
   }
 })
   .then(function (response) {
